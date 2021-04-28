37a2d0e9340c2d091fe1c3ab3bb16d07aae77cd8
YouTubePlays <allplayslive@gmail.com>
2021-04-27 22:01:05 -0700

    fix: add write secure setting in case the `setBluetoothClass` api is broken
    This commit adds:
    - auto enable if the bluetooth is off
    - adds write secure setting if the bluetooth api is broken

--- app/src/main/java/com/deepbluelabs/bpp/MainActivity.java
+++ app/src/main/java/com/deepbluelabs/bpp/MainActivity.java
@@ -2,9 +2,14 @@ package com.deepbluelabs.bpp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothClass;
-import android.content.DialogInterface;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -23,10 +28,13 @@ import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getName();
+
     private TextView deviceClassTextView;
     private ImageButton deviceClassButton;
 
     private BluetoothAdapter bluetoothAdapter;
+    private BluetoothBroadcastReceiver bluetoothBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,24 +44,62 @@ public class MainActivity extends AppCompatActivity {
         deviceClassTextView = findViewById(R.id.device_class);
         deviceClassButton = findViewById(R.id.device_class_button);
 
+        //Register bluetooth turn on receiver
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        bluetoothBroadcastReceiver = new BluetoothBroadcastReceiver();
+        registerReceiver(bluetoothBroadcastReceiver, filter);
+
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (Objects.nonNull(bluetoothAdapter)) {
-            BluetoothClass bluetoothClass = bluetoothAdapter.getBluetoothClass();
-            if (Objects.nonNull(bluetoothClass)) {
-                deviceClassTextView.setText(HexDump.toHexString(bluetoothClass.getClassOfDevice()));
+            if (bluetoothAdapter.isEnabled()) {
+                fillDeviceClass();
+            } else {
+                bluetoothAdapter.enable();
             }
+        } else {
+            showAdapterNotFoundToast();
         }
 
         deviceClassButton.setOnClickListener(this::showDeviceClassSelector);
     }
 
+    private class BluetoothBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_OFF:
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        fillDeviceClass();
+                        break;
+                    case BluetoothAdapter.STATE_TURNING_ON:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void fillDeviceClass() {
+        BluetoothClass bluetoothClass = Optional.ofNullable(bluetoothAdapter.getBluetoothClass())
+                .orElseGet(() -> new BluetoothClass(retrieveBluetoothClassConfig()));
+        if (Objects.nonNull(bluetoothClass)) {
+            deviceClassTextView.setText(HexDump.toHexString(bluetoothClass.getClassOfDevice()));
+        }
+    }
 
     private void showDeviceClassSelector(View view) {
         AlertDialog.Builder builderSingle = new AlertDialog.Builder(this);
         builderSingle.setIcon(R.drawable.ic_baseline_settings_24);
         builderSingle.setTitle(R.string.select_device_class);
 
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_singlechoice);
         arrayAdapter.add(getString(R.string.gamepad));
         arrayAdapter.add(getString(R.string.phone));
         arrayAdapter.add(getString(R.string.custom));
@@ -124,17 +170,12 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
     }
 
-    private void showSuccessToast() {
-        Toast.makeText(this, R.string.device_class_set, Toast.LENGTH_SHORT).show();
-    }
-
-    private void showFailToast() {
-        Toast.makeText(this, R.string.could_not_set_device_class, Toast.LENGTH_SHORT).show();
-    }
-
     private boolean setBluetoothDeviceClass(int deviceClass) {
         try {
             boolean result = bluetoothAdapter.setBluetoothClass(new BluetoothClass(deviceClass));
+            if (!result) {
+                result = storeBluetoothClassConfig(deviceClass);
+            }
             if (result) {
                 deviceClassTextView.setText(HexDump.toHexString(deviceClass));
             }
@@ -144,4 +185,33 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
+
+    private int retrieveBluetoothClassConfig() {
+        return Settings.Global.getInt(
+                getContentResolver(), Settings.Global.BLUETOOTH_CLASS_OF_DEVICE, 0);
+    }
+
+    private boolean storeBluetoothClassConfig(int bluetoothClass) {
+        boolean result = Settings.Global.putInt(
+                getContentResolver(), Settings.Global.BLUETOOTH_CLASS_OF_DEVICE, bluetoothClass);
+
+        if (!result) {
+            Log.e(TAG, "Error storing BluetoothClass config - " + bluetoothClass);
+        }
+
+        return result;
+    }
+
+    private void showSuccessToast() {
+        Toast.makeText(this, R.string.device_class_set, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showFailToast() {
+        Toast.makeText(this, R.string.could_not_set_device_class, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showAdapterNotFoundToast() {
+        Toast.makeText(this, R.string.bluetooth_adapter_not_found, Toast.LENGTH_SHORT).show();
+    }
+
 }
