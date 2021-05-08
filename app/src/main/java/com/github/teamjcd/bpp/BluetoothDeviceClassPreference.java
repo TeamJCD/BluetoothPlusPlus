5e73427575d27ccaa30933f283fe1d8811665818
Danny Trunk <dtrunk90@gmail.com>
2021-05-08 23:30:51 +0200

    minor refactoring

--- app/src/main/java/com/github/teamjcd/bpp/BluetoothDeviceClassPreference.java
+++ app/src/main/java/com/github/teamjcd/bpp/BluetoothDeviceClassPreference.java
@@ -33,7 +33,7 @@ public class BluetoothDeviceClassPreference extends Preference implements Compou
 
     public BluetoothDeviceClassPreference(Context context, AttributeSet attrs) {
         this(context, attrs, androidx.preference.R.attr.preferenceFragmentCompatStyle);
-        setWidgetLayoutResource(R.layout.bluetooth_device_class_preference_layout);
+        setWidgetLayoutResource(R.layout.widget_bluetooth_device_class_preference);
     }
 
     public BluetoothDeviceClassPreference(Context context) {
